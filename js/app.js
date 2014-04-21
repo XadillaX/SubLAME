3c61da4b329c53e33a8991da63846d4384a70f49
Ib Quezada <ib@ibquezada.com>
2014-04-21 00:24:03 -0700

    Fix theme behavior

--- js/app.js
+++ js/app.js
@@ -1,10 +1,23 @@
-var gui = require('nw.gui'),
-  	win = gui.Window.get();
-  	win.showDevTools();
+// var gui = require('nw.gui'),
+//   	win = gui.Window.get();
+//   	win.showDevTools();
+
 
 window.onload = function () {
-	var inferiorText = document.getElementById('inferior-text'),
-		myCodeMirror = CodeMirror.fromTextArea(inferiorText, {
-			lineNumbers: true
-		});
+	var sublameText = document.getElementById('sublame-text'),
+		myCodeMirror = null;
+
+
+
+	myCodeMirror = CodeMirror.fromTextArea(sublameText, {
+		lineNumbers: true,
+		styleActiveLine: true,
+    	matchBrackets: true
+	});
+	myCodeMirror.setOption('theme', 'ambiance');
+	myCodeMirror.setOption('mode', 'htmlmixed');
+
+	
+
+	myCodeMirror.focus();
 };
