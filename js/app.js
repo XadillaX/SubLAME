968c3155a5ffd09919518c1cb28d3af0befc3c58
Ib Quezada <ib@ibquezada.com>
2014-04-21 23:22:46 -0700

    Add file open/save functionality

--- js/app.js
+++ js/app.js
@@ -4,23 +4,56 @@
 
 
 window.onload = function () {
-    function saveFile(name, text) {
-        var chooser = document.querySelector(name);
-        chooser.addEventListener("change", function(evt) {
-            fs.writeFile(this.value, text, function(err) {
-                if(err) {
-                    alert("error");
-                }
-            });
-        }, false);
+	function writeFile(fileName, text) {
+        fs.writeFile(fileName, text, function(err) {
+            if(err) {
+                alert("error");
+            }
+        });
+	}
 
-        chooser.click();  
+    function openFile(fileName) {
+        fs.readFile(fileName, 'utf-8', function (error, contents) {
+            myCodeMirror.setValue(contents);
+            myCodeMirror.setOption('isNewFile', false);
+            myCodeMirror.setOption('fileName', fileName);
+        }); 
+    }
+
+    function saveDialog(name, text) {
+        var chooser = document.querySelector(name),
+            fileName = null;
+
+        console.log('isNewFile', myCodeMirror.getOption('isNewFile'));
+        
+        if (myCodeMirror.getOption('isNewFile')) {
+	        chooser.addEventListener("change", function(evt) {
+                fileName = this.value;
+                writeFile(fileName, text);
+                myCodeMirror.setOption('isNewFile', false);
+                myCodeMirror.setOption('fileName', fileName);
+                console.log(myCodeMirror.getOption('fileName'));
+	        }, false);
+            chooser.click(); 
+	    } else {
+            writeFile(myCodeMirror.getOption('fileName'), myCodeMirror.getValue());
+	    }
+    }
+
+    function openDialog(name) {
+        var chooser = document.querySelector(name),
+            fileName = null;
+        chooser.addEventListener("change", function(evt) {
+            fileName = this.value;
+            openFile(fileName);
+        }, false);
+        chooser.click();
     }
 
 
     var sublameText, myCodeMirror,
         gui = require('nw.gui'), //or global.window.nwDispatcher.requireNwGui() (see https://github.com/rogerwang/node-webkit/issues/707)
-        // fs = require('js'),
+        fs = require('fs'),
         win = gui.Window.get(); // Get the current window
 
     sublameText = document.getElementById('sublame-text');
@@ -36,11 +69,14 @@ window.onload = function () {
                 win.toggleFullscreen()
             },
             "Ctrl-S": function (cm) {
-                alert(this.value);
-                saveFile('#fileDialog', this.value);
+                saveDialog('#fileDialog', cm.getValue());
+            },
+            "Ctrl-O": function (cm) {
+                openDialog('#openDialog');
             }
         }
     });
+    myCodeMirror.setOption('isNewFile', true);
 
     myCodeMirror.setOption('theme', 'ambiance');
     myCodeMirror.setOption('mode', 'htmlmixed');
