7b95ce2b063c1504512abb9473ab1a6405242b57
iquezada <iquezada@switchfly.com>
2014-04-22 01:55:40 +0000

    Add file dialogs

--- js/app.js
+++ js/app.js
@@ -4,23 +4,42 @@
 
 
 window.onload = function () {
+    function saveFile(name, text) {
+        var chooser = document.querySelector(name);
+        chooser.addEventListener("change", function(evt) {
+            fs.writeFile(this.value, text, function(err) {
+                if(err) {
+                    alert("error");
+                }
+            });
+        }, false);
+
+        chooser.click();  
+    }
+
 
     var sublameText, myCodeMirror,
         gui = require('nw.gui'), //or global.window.nwDispatcher.requireNwGui() (see https://github.com/rogerwang/node-webkit/issues/707)
+        // fs = require('js'),
         win = gui.Window.get(); // Get the current window
 
     sublameText = document.getElementById('sublame-text');
 
+
     myCodeMirror = CodeMirror.fromTextArea(sublameText, {
-	lineNumbers: true,
-	styleActiveLine: true,
-    	matchBrackets: true,
+    lineNumbers: true,
+    styleActiveLine: true,
+        matchBrackets: true,
         extraKeys: {
-        "F11": function (cm) {
-            console.log(win);
-            win.toggleFullscreen()
+            "F11": function (cm) {
+                console.log(win);
+                win.toggleFullscreen()
+            },
+            "Ctrl-S": function (cm) {
+                alert(this.value);
+                saveFile('#fileDialog', this.value);
+            }
         }
-    }
     });
 
     myCodeMirror.setOption('theme', 'ambiance');
